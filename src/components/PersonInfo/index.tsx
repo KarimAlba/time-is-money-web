e8a1e191e5dce6955e6422990fb34253cc1633c1
danilZherdev <107792683+danilZherdev@users.noreply.github.com>
2023-09-16 21:31:52 +0300

    add sidebar user

--- src/components/PersonInfo/index.tsx
+++ src/components/PersonInfo/index.tsx
@@ -6,6 +6,9 @@ import ArrowDownIcon from '../../assets/imgTimeIsMoney/arrow-down-icon.svg'
 import { Link } from 'react-router-dom';
 import ClientAccountAPI from "../../api/ClientAccountingAPI";
 import IEntityResponse from "../../models/response/IEntityResponse";
+import house from '../../assets/imgTimeIsMoney/house.svg'
+import crossBurgerMenu from '../../assets/imgTimeIsMoney/crossBurgerMenu.svg'
+
 
 interface PersonInfoPropsTypes {
     handleIsOpenFooter: Function
@@ -27,6 +30,8 @@ const PersonInfo = (props: PersonInfoPropsTypes) => {
     const [templateCount3, setTemplateCount3] = useState<boolean>(false)
 
     const { handleIsOpenFooter, handleOpenPlugin } = props;
+
+    const [menuActive, setMenuActive] = useState<boolean>(false);
 
     const handleCheckBox = () => {
         setIsDownload(!isDownload);
@@ -85,129 +90,261 @@ const PersonInfo = (props: PersonInfoPropsTypes) => {
     }, [])
 
     return (
-        <div className="person-info">
-            <div className="person-info_title">
-                <p>{title}</p>
-                <span>{email}</span>
-                <Link to='edit-user'>Редактировать данные</Link>
-            </div>
-            <div className="person-info_statistic">
-                <div className="statistic_point">
-                    <p>дата регистрации:</p>
-                    <span>{(new Date(createdAt)).toLocaleDateString()}</span>
+        <div>
+            {menuActive
+                ? (<div className="modalHouse">
+                    <img className="heder-cross" src={crossBurgerMenu} onClick={() => setMenuActive(false)} />
+                    <div className="menuHouse">
+                        <div className="person-info_title">
+                            <p>{title}</p>
+                            <span>{email}</span>
+                            <Link to='edit-user'>Редактировать данные</Link>
+                        </div>
+                        <div className="person-info_statistic">
+                            <div className="statistic_point">
+                                <p>дата регистрации:</p>
+                                <span>{(new Date(createdAt)).toLocaleDateString()}</span>
+                            </div>
+                            <div className="statistic_point">
+                                <p>скачено плагинов:</p>
+                                <span>0</span>
+                            </div>
+                            <div className="statistic_value">
+                                <p>количество</p>
+                                <div
+                                    onClick={handleTemplateClick}
+                                    className="statistic_point"
+                                >
+                                    <div style={{ display: 'flex' }}>
+                                        {templateCount
+                                            ? <img src={ArrowDownIcon} />
+                                            : <img src={ArrowIcon} />
+                                        }
+                                        <p>заполненных шаблонов:</p>
+                                    </div>
+                                    {templateCount
+                                        ? (<div className="select_point">
+                                            <p>В разработке</p>
+                                        </div>)
+                                        : null
+                                    }
+                                </div>
+                                <div
+                                    onClick={handleTemplateClick2}
+                                    className="statistic_point">
+                                    <div style={{ display: 'flex' }}>
+                                        {templateCount2
+                                            ? <img src={ArrowDownIcon} />
+                                            : <img src={ArrowIcon} />
+                                        }
+                                        <p>заполненных приложений:</p>
+                                    </div>
+                                    {templateCount2
+                                        ? (<div className="select_point">
+                                            <p>В разработке</p>
+                                        </div>)
+                                        : null
+                                    }
+                                </div>
+                                <div
+                                    onClick={handleTemplateClick3}
+                                    className="statistic_point">
+                                    <div style={{ display: 'flex' }}>
+                                        {templateCount3
+                                            ? <img src={ArrowDownIcon} />
+                                            : <img src={ArrowIcon} />
+                                        }
+                                        <p>QR-code действителен до:</p>
+                                    </div>
+                                    {templateCount3
+                                        ? (<div className="select_point">
+                                            <p>В разработке</p>
+                                        </div>)
+                                        : null
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        <div className="person-info_license">
+                            <button
+                                onClick={handleDownload}
+                                className="download-button"
+                                style={isDownload
+                                    ? { backgroundColor: '#00b2f4' }
+                                    : { backgroundColor: '#00b2f447' }
+                                }
+                            >
+                                скачать
+                            </button>
+                            <div className="agreement">
+                                <button onClick={handleCheckBox} className="checkbox-button">
+                                    {isDownload
+                                        ? <img src={DoneIcon} />
+                                        : null
+                                    }
+                                </button>
+                                <p>
+                                    <span
+                                        style={isDownload
+                                            ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                            : { color: 'black' }
+                                        }
+                                    >
+                                        настоящим я соглашаюсь с
+                                    </span>
+                                    <a> политикой конфиденциальности </a>
+                                    <span
+                                        style={isDownload
+                                            ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                            : { color: 'black' }
+                                        }
+                                    >
+                                        и
+                                    </span>
+                                    <br />
+                                    <a>
+                                        правилами пользования
+                                    </a>
+                                    <span
+                                        style={isDownload
+                                            ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                            : { color: 'black' }
+                                        }
+                                    >
+                                        плагином
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>)
+                : <img className="heder-house" src={house} onClick={() => setMenuActive(true)} />
+            }
+            <div className="person-info">
+                <div className="person-info_title">
+                    <p>{title}</p>
+                    <span>{email}</span>
+                    <Link to='edit-user'>Редактировать данные</Link>
                 </div>
-                <div className="statistic_point">
-                    <p>скачено плагинов:</p>
-                    <span>0</span>
-                </div>
-                <div className="statistic_value">
-                    <p>количество</p>
-                    <div
-                        onClick={handleTemplateClick}
-                        className="statistic_point"
-                    >
-                        <div style={{ display: 'flex' }}>
+                <div className="person-info_statistic">
+                    <div className="statistic_point">
+                        <p>дата регистрации:</p>
+                        <span>{(new Date(createdAt)).toLocaleDateString()}</span>
+                    </div>
+                    <div className="statistic_point">
+                        <p>скачено плагинов:</p>
+                        <span>0</span>
+                    </div>
+                    <div className="statistic_value">
+                        <p>количество</p>
+                        <div
+                            onClick={handleTemplateClick}
+                            className="statistic_point"
+                        >
+                            <div style={{ display: 'flex' }}>
+                                {templateCount
+                                    ? <img src={ArrowDownIcon} />
+                                    : <img src={ArrowIcon} />
+                                }
+                                <p>заполненных шаблонов:</p>
+                            </div>
                             {templateCount
-                                ? <img src={ArrowDownIcon} />
-                                : <img src={ArrowIcon} />
+                                ? (<div className="select_point">
+                                    <p>В разработке</p>
+                                </div>)
+                                : null
                             }
-                            <p>заполненных шаблонов:</p>
                         </div>
-                        {templateCount
-                            ? (<div className="select_point">
-                                <p>В разработке</p>
-                            </div>)
-                            : null
-                        }
-                    </div>
-                    <div
-                        onClick={handleTemplateClick2}
-                        className="statistic_point">
-                        <div style={{ display: 'flex' }}>
+                        <div
+                            onClick={handleTemplateClick2}
+                            className="statistic_point">
+                            <div style={{ display: 'flex' }}>
+                                {templateCount2
+                                    ? <img src={ArrowDownIcon} />
+                                    : <img src={ArrowIcon} />
+                                }
+                                <p>заполненных приложений:</p>
+                            </div>
                             {templateCount2
-                                ? <img src={ArrowDownIcon} />
-                                : <img src={ArrowIcon} />
+                                ? (<div className="select_point">
+                                    <p>В разработке</p>
+                                </div>)
+                                : null
                             }
-                            <p>заполненных приложений:</p>
                         </div>
-                        {templateCount2
-                            ? (<div className="select_point">
-                                <p>В разработке</p>
-                            </div>)
-                            : null
-                        }
-                    </div>
-                    <div
-                        onClick={handleTemplateClick3}
-                        className="statistic_point">
-                        <div style={{ display: 'flex' }}>
+                        <div
+                            onClick={handleTemplateClick3}
+                            className="statistic_point">
+                            <div style={{ display: 'flex' }}>
+                                {templateCount3
+                                    ? <img src={ArrowDownIcon} />
+                                    : <img src={ArrowIcon} />
+                                }
+                                <p>QR-code действителен до:</p>
+                            </div>
                             {templateCount3
-                                ? <img src={ArrowDownIcon} />
-                                : <img src={ArrowIcon} />
+                                ? (<div className="select_point">
+                                    <p>В разработке</p>
+                                </div>)
+                                : null
                             }
-                            <p>QR-code действителен до:</p>
                         </div>
-                        {templateCount3
-                            ? (<div className="select_point">
-                                <p>В разработке</p>
-                            </div>)
-                            : null
-                        }
                     </div>
                 </div>
-            </div>
-            <div className="person-info_license">
-                <button
-                    onClick={handleDownload}
-                    className="download-button"
-                    style={isDownload
-                        ? { backgroundColor: '#00b2f4' }
-                        : { backgroundColor: '#00b2f447' }
-                    }
-                >
-                    скачать
-                </button>
-                <div className="agreement">
-                    <button onClick={handleCheckBox} className="checkbox-button">
-                        {isDownload
-                            ? <img src={DoneIcon} />
-                            : null
+                <div className="person-info_license">
+                    <button
+                        onClick={handleDownload}
+                        className="download-button"
+                        style={isDownload
+                            ? { backgroundColor: '#00b2f4' }
+                            : { backgroundColor: '#00b2f447' }
                         }
+                    >
+                        скачать
                     </button>
-                    <p>
-                        <span
-                            style={isDownload
-                                ? { color: 'rgba(0, 0, 0, 0.3)' }
-                                : { color: 'black' }
+                    <div className="agreement">
+                        <button onClick={handleCheckBox} className="checkbox-button">
+                            {isDownload
+                                ? <img src={DoneIcon} />
+                                : null
                             }
-                        >
-                            настоящим я соглашаюсь с
-                        </span>
-                        <a> политикой конфиденциальности </a>
-                        <span
-                            style={isDownload
-                                ? { color: 'rgba(0, 0, 0, 0.3)' }
-                                : { color: 'black' }
-                            }
-                        >
-                            и
-                        </span>
-                        <br />
-                        <a>
-                            правилами пользования
-                        </a>
-                        <span
-                            style={isDownload
-                                ? { color: 'rgba(0, 0, 0, 0.3)' }
-                                : { color: 'black' }
-                            }
-                        >
-                            плагином
-                        </span>
-                    </p>
+                        </button>
+                        <p>
+                            <span
+                                style={isDownload
+                                    ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                    : { color: 'black' }
+                                }
+                            >
+                                настоящим я соглашаюсь с
+                            </span>
+                            <a> политикой конфиденциальности </a>
+                            <span
+                                style={isDownload
+                                    ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                    : { color: 'black' }
+                                }
+                            >
+                                и
+                            </span>
+                            <br />
+                            <a>
+                                правилами пользования
+                            </a>
+                            <span
+                                style={isDownload
+                                    ? { color: 'rgba(0, 0, 0, 0.3)' }
+                                    : { color: 'black' }
+                                }
+                            >
+                                плагином
+                            </span>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+
     )
 }
 
