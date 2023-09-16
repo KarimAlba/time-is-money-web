7318bd93135a9991b564c36f7ba6ef72d574a33f
Acer <karim_skiy@mail.ru>
2023-09-16 19:32:21 +0300

    auth and registration updated

--- src/api/OrganizationAPI.ts
+++ src/api/OrganizationAPI.ts
@@ -1,10 +1,15 @@
 import { AxiosResponse } from "axios";
 import axiosConfig from "./axiosConfig";
+import IEditOrganizRequest from "../models/request/IEditOrganizRequest";
 import IOrganizationRequest from "../models/request/IOrganizationRequest";
 
 class OrganizationAPI{
     public static registration(body: IOrganizationRequest): Promise<AxiosResponse<any | any>> {
         return axiosConfig.post("/organization/registration/", {...body});
+    }
+
+    public static edit(body: IEditOrganizRequest): Promise<AxiosResponse<any | any>> {
+        return axiosConfig.patch("/organization", {...body});
     }
 }
 
--- src/components/EditUser/index.tsx
+++ src/components/EditUser/index.tsx
@@ -1,5 +1,7 @@
 import './style.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import OrganizationAPI from '../../api/OrganizationAPI';
 import PhysicalAccountAPI from '../../api/PhysicalAccountAPI';
 
 const EditUser = () => {
@@ -12,48 +14,71 @@ const EditUser = () => {
     const [email, setEmail] = useState<string>('');
     const [confirmEmail, setConfirmEmail] = useState<string>('');
 
-    const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLastname(e.target.value);
+    const [orgName, setOrgName] = useState<string>(''); 
+    const [orgAddress, setOrgAddress] = useState<string>('');
+    const [orgINN, setOrgINN] = useState<string>('');
+    const [orgKPP, setOrgKPP] = useState<string>('');
+
+    const navigate = useNavigate();
+
+    const handleLastNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setLastname(e.target.value);
+
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value);
+
+    const handlePatronymicChange = (e: React.ChangeEvent<HTMLInputElement>) => setPatronymic(e.target.value);
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value);
+
+    const handleConfirmEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => setConfirmEmail(e.target.value);
+
+    const handleOrgNameChange = (e: React.ChangeEvent<HTMLInputElement>) => setOrgName(e.target.value);
+    
+    const handleOrgAddressChange = (e: React.ChangeEvent<HTMLInputElement>) => setOrgAddress(e.target.value);
+
+    const handleOrgINNChange = (e: React.ChangeEvent<HTMLInputElement>) => setOrgINN(e.target.value);
+
+    const handleOrgKPPChange = (e: React.ChangeEvent<HTMLInputElement>) => setOrgKPP(e.target.value);
+
+    const getData = () => {
+        if (String(localStorage.getItem('id'))) {
+            setId(String(localStorage.getItem('id')));
+        }
+
+        if (String(localStorage.getItem('name'))) {
+            setName(String(localStorage.getItem('name')));
+        }
+
+        if (String(localStorage.getItem('email'))) {
+            setEmail(String(localStorage.getItem('email')));
+            setConfirmEmail(String(localStorage.getItem('email')));
+        }
+
+        if (String(localStorage.getItem('lastname'))) {
+            setLastname(String(localStorage.getItem('lastname')));
+        }
+
+        if (String(localStorage.getItem('patronymic'))) {
+            setPatronymic(String(localStorage.getItem('patronymic')));
+        }
     }
 
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value);
+    const getOrgData = () => {
+        if (String(localStorage.getItem('organizationName'))) {
+            setOrgName(String(localStorage.getItem('organizationName')));
+        }
+
+        if (String(localStorage.getItem('organizationAddress'))) {
+            setOrgAddress(String(localStorage.getItem('organizationAddress')));
+        }
+
+        if (String(localStorage.getItem('inn'))) {
+            setOrgINN(String(localStorage.getItem('inn')));
+        }
+
+        if (String(localStorage.getItem('kpp'))) {
+            setOrgKPP(String(localStorage.getItem('kpp')));
+        }
     }
-
-    const handlePatronymicChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPatronymic(e.target.value);
-    }
-
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    }
-
-    const handleConfirmEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setConfirmEmail(e.target.value);
-    }
-
-    // const getDataPhysical = () => {
-    //     if (String(localStorage.getItem('id'))) {
-    //         setId(String(localStorage.getItem('id')));
-    //     }
-
-    //     if (String(localStorage.getItem('name'))) {
-    //         setName(String(localStorage.getItem('name')));
-    //     }
-
-    //     if (String(localStorage.getItem('email'))) {
-    //         setEmail(String(localStorage.getItem('email')));
-    //         setConfirmEmail(String(localStorage.getItem('email')));
-    //     }
-
-    //     if (String(localStorage.getItem('lastname'))) {
-    //         setLastname(String(localStorage.getItem('lastname')));
-    //     }
-
-    //     if (String(localStorage.getItem('patronymic'))) {
-    //         setPatronymic(String(localStorage.getItem('patronymic')));
-    //     }
-    // }
 
     const handleAccessPhysical = () => {
         if (!lastname && !name && !patronymic
@@ -67,12 +92,41 @@ const EditUser = () => {
                 patronymic: patronymic,
                 email: email
             }
-
-            PhysicalAccountAPI.edit(id, edittedUser)
+            console.log(edittedUser);
+            PhysicalAccountAPI.edit(edittedUser)
                 .then(response => {
                     console.log(response);
                     if (response.status < 400) {
+                        navigate('/login')
+                    }
+                })
+                .catch(error => console.log(error))
+        }
+    }
 
+    const handleAccessOrganization = () => {
+        if (!lastname && !name && !patronymic
+            && !email && !confirmEmail && !orgAddress
+            && !orgINN && !orgKPP
+        ) {
+            return
+        } else {
+            const edittedOrg = {
+                lastname: lastname,
+                name: name,
+                patronymic: patronymic,
+                email: email,
+                organizationName: orgName,
+                address: orgAddress,
+                inn: orgINN,
+                kpp: orgKPP,
+            }
+            console.log(edittedOrg);
+            OrganizationAPI.edit(edittedOrg)
+                .then(response => {
+                    console.log(response);
+                    if (response.status < 400) {
+                        navigate('/login')
                     }
                 })
                 .catch(error => console.log(error))
@@ -125,37 +179,38 @@ const EditUser = () => {
             </button>
         </div>
 
-const organizationBlock = <div>
+const organizationBlock = 
+    <div>
         <div className="edit-user_data">
             <h5>Наименование организации</h5>
             <input 
                 type="text" 
-                defaultValue={lastname ? lastname : ''}
-                onInput={handleLastNameChange}
+                defaultValue={orgName ? orgName : ''}
+                onInput={handleOrgNameChange}
             />
         </div>
         <div className="edit-user_data">
             <h5>Юридический адрес</h5>
             <input 
                 type="text" 
-                defaultValue={name ? name : ''}
-                onInput={handleNameChange}
+                defaultValue={orgAddress ? orgAddress : ''}
+                onInput={handleOrgAddressChange}
             />
         </div>
         <div className="edit-user_data">
             <h5>ИНН</h5>
             <input 
                 type="text" 
-                defaultValue={patronymic ? patronymic : ''}
-                onInput={handlePatronymicChange}
+                defaultValue={orgINN ? orgINN : ''}
+                onInput={handleOrgINNChange}
             />
         </div>
         <div className="edit-user_data">
             <h5>КПП</h5>
             <input 
                 type="text" 
-                defaultValue={email ? email : ''}
-                onInput={handleEmailChange}
+                defaultValue={orgKPP ? orgKPP : ''}
+                onInput={handleOrgKPPChange}
             />
         </div>
         <div className="edit-user_data">
@@ -166,50 +221,52 @@ const organizationBlock = <div>
                 onInput={handleLastNameChange}
             />
         </div>
-            <div className="edit-user_data">
-                <h5>Руководитель организации:   Фамилия</h5>
-                <input 
-                    type="text" 
-                    defaultValue={lastname ? lastname : ''}
-                    onInput={handleLastNameChange}
-                />
-            </div>
-            <div className="edit-user_data">
-                <h5>Имя</h5>
-                <input 
-                    type="text" 
-                    defaultValue={name ? name : ''}
-                    onInput={handleNameChange}
-                />
-            </div>
-            <div className="edit-user_data">
-                <h5>Отчество</h5>
-                <input 
-                    type="text" 
-                    defaultValue={patronymic ? patronymic : ''}
-                    onInput={handlePatronymicChange}
+        <div className="edit-user_data">
+            <h5>Имя</h5>
+            <input 
+                type="text" 
+                defaultValue={name ? name : ''}
+                onInput={handleNameChange}
             />
-            </div>
-            <div className="edit-user_data">
-                <h5>Введите новый e-mail</h5>
-                <input 
-                    type="text" 
-                    defaultValue={email ? email : ''}
-                    onInput={handleEmailChange}
-                />
-            </div>
-            <div className="edit-user_data">
-                <h5>Подтвердите e-mail</h5>
-                <input 
-                    type="text" 
-                    defaultValue={confirmEmail ? confirmEmail : ''}
-                    onInput={handleConfirmEmailChange}
-                />
-            </div>
-        <button onClick={handleAccessPhysical}>
+        </div>
+        <div className="edit-user_data">
+            <h5>Отчество</h5>
+            <input 
+                type="text" 
+                defaultValue={patronymic ? patronymic : ''}
+                onInput={handlePatronymicChange}
+        />
+        </div>
+        <div className="edit-user_data">
+            <h5>Введите новый e-mail</h5>
+            <input 
+                type="text" 
+                defaultValue={email ? email : ''}
+                onInput={handleEmailChange}
+            />
+        </div>
+        <div className="edit-user_data">
+            <h5>Подтвердите e-mail</h5>
+            <input 
+                type="text" 
+                defaultValue={confirmEmail ? confirmEmail : ''}
+                onInput={handleConfirmEmailChange}
+            />
+        </div>
+        <button onClick={handleAccessOrganization}>
             ПОДТВЕРДИТЬ 
         </button>
     </div>
+
+    useEffect(() => {
+        getData();
+        if (localStorage.getItem('inn')) {
+            setType(0)
+            getOrgData();
+        } else {
+            setType(1);
+        }
+    }, [])
 
     return (
         <div className="edit-user">
