ee6df9cf9f15e54f3c2b693dd150f21e456f2560
Acer <karim_skiy@mail.ru>
2023-09-27 15:39:31 +0300

    qr-code downloading

--- src/api/WorkStationAPI.ts
+++ src/api/WorkStationAPI.ts
@@ -1,5 +1,6 @@
 import { AxiosResponse } from "axios";
 import axiosConfig from "./axiosConfig";
+import IWorkStationUpdateRequest from "../models/request/IWorkStationUpdateRequest";
 
 class WorkStationtAPI{
     public static search(param: string): Promise<AxiosResponse<any | any>> {
@@ -11,7 +12,13 @@ class WorkStationtAPI{
     }
 
     public static getQRCode(id: number): Promise<AxiosResponse<any | any>> {
-        return axiosConfig.get(`/api/main/work-station/read/file/qr?id=${id}`);
+        return axiosConfig.get(`/api/main/work-station/read/file/qr?id=${id}`, {
+            responseType: "blob"
+        });
+    }
+
+    public static update(id: number, body: IWorkStationUpdateRequest): Promise<AxiosResponse<any | any>> {
+        return axiosConfig.patch(`/api/main/work-station/update?id=${id}`, { ...body });
     }
 }
 
