ef84fbe352c2e303bca74c5a3c5c54cf708025de
Acer <karim_skiy@mail.ru>
2023-09-27 16:31:19 +0300

    prolongation logical

--- src/api/WorkStationAPI.ts
+++ src/api/WorkStationAPI.ts
@@ -1,24 +1,25 @@
 import { AxiosResponse } from "axios";
 import axiosConfig from "./axiosConfig";
 import IWorkStationUpdateRequest from "../models/request/IWorkStationUpdateRequest";
+import IErrorResponse from "../models/response/IErrorResponse";
 
 class WorkStationtAPI{
-    public static search(param: string): Promise<AxiosResponse<any | any>> {
+    public static search(param: string): Promise<AxiosResponse<any | IErrorResponse>> {
         return axiosConfig.get(`/api/main/work-station/search-by-id-or-name?searchParam=${param}`);
     }
 
-    public static getStatistic(): Promise<AxiosResponse<any | any>> {
+    public static getStatistic(): Promise<AxiosResponse<any | IErrorResponse>> {
         return axiosConfig.get('/api/main/work-station/find-statistic');
     }
 
-    public static getQRCode(id: number): Promise<AxiosResponse<any | any>> {
+    public static getQRCode(id: number): Promise<AxiosResponse<any | IErrorResponse>> {
         return axiosConfig.get(`/api/main/work-station/read/file/qr?id=${id}`, {
             responseType: "blob"
         });
     }
 
-    public static update(id: number, body: IWorkStationUpdateRequest): Promise<AxiosResponse<any | any>> {
-        return axiosConfig.patch(`/api/main/work-station/update?id=${id}`, { ...body });
+    public static prolongation(id: number): Promise<AxiosResponse<any | IErrorResponse>> {
+        return axiosConfig.patch(`/api/main/work-station/prolongation?month=1&id=${id}`);
     }
 }
 
