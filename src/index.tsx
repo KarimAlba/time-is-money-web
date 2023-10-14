d5d3207c29c60728007cef093d768d25e3d67699
Karim <karim_skiy@mail.ru>
2023-10-14 14:23:03 +0300

    'refactoring'

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,11 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <App/>
       </BrowserRouter>
   </Provider>
 );
