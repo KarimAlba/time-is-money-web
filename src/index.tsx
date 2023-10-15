8b6320bb6e80217e9e47edd24c724280e0168f07
Karim <karim_skiy@mail.ru>
2023-10-15 11:50:10 +0300

    fixed bag with global state

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,28 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import { render } from 'react-dom'
+//import { createStore } from 'redux'
+//import reducer from './store/reducers';
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// root.render(
+//   <Provider store={store}>
+//       <BrowserRouter>
+//         <App/>
+//       </BrowserRouter>
+//   </Provider>
+// );
 
-root.render(
+// const store = createStore(rootReducer)
+
+render(
   <Provider store={store}>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
-  </Provider>
-);
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
