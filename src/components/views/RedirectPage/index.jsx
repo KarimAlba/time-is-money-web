38297d647ad1730c874e8c51cdbf7e9b1a64d276
Карим Фаткуллин <dilkar7685@gmail.com>
2023-11-24 21:00:59 +0300

    redirect logical initted

---
+++ src/components/views/RedirectPage/index.jsx
@@ -0,0 +1,63 @@
+import './style.css';
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import redirectConfig from '../../../utils/redirect';
+import logo from '../../../assets/imgTimeIsMoney/ТиМ2.svg';
+
+const RedirectPage = (props) => {
+    const { handleRedirectPageMounted } = props;
+
+    const navigate = useNavigate();
+
+    const AppRedirect = redirectConfig();
+    
+    var qs = AppRedirect.queryString;
+
+    // Here we initiate the redirect process
+    AppRedirect.redirect({
+
+        iosApp: 'tim://plugin/' + qs['uuid'],
+
+        iosAppStore: 'https://apps.apple.com/us/app/%D1%82%D0%B8%D0%BC/id6447686674' + qs['message'],
+
+        // For this, your app need to have category filter: android.intent.category.BROWSABLE
+
+        android: {
+
+            'host': 'plugin/' + encodeURIComponent(qs['uuid']), // Host/path/querystring part in a custom scheme URL
+            // 'action': '', // Equivalent to ACTION in INTENTs
+            // 'category': '', // Equivalent to CATEGORY in INTENTs
+            // 'component': '', // Equivalent to COMPONENT in INTENTs
+            'scheme': 'timeismoney', // Scheme part in a custom scheme URL
+            'package': 'com.studiovr.timeismoney', // Package name in Play store
+            'fallback': 'https://play.google.com/store/apps/details?id=com.studiovr.timeismoney'
+        }
+
+    }); 
+
+    useEffect(() => {
+        handleRedirectPageMounted(false)
+    });
+
+    return (
+        <div className='redirect-container'>
+            <script src='../../../utils/redirect'></script>
+            <img 
+                src={logo} 
+                alt="logotype" 
+                onClick={() => navigate(-1)}
+            />
+
+            <div className='redirect-container_text'>
+                <p>
+                    This is the automatic redirect page to the native app or to the App Store / Play Store.
+                </p>
+                <span>
+                    Сайт использует cookie для своей работы. Если вы не согласны, покиньте сайт.
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default RedirectPage; 
