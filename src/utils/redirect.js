602e0a7b6202774e8358a69102edc51024ca9b50
Карим Фаткуллин <dilkar7685@gmail.com>
2023-12-05 12:51:47 +0300

    ios redirecting logical

--- src/utils/redirect.js
+++ src/utils/redirect.js
@@ -26,7 +26,6 @@ function redirectConfig() {
         queryString: queryString,
 
         redirect: function (options) {
-            console.log('options', options)
             var hasIos = !!(options.iosApp || options.iosAppStore);
             var hasAndroid = !!(options.android);
             var hasOverallFallback = !!(options.overallFallback);
@@ -63,13 +62,12 @@ function redirectConfig() {
             };
 
             if (hasIos && /iP(hone|ad|od)/.test(navigator.userAgent)) {
-                var urls = [];
-                if (options.iosApp) {
-                    urls.push(options.iosApp);
-                } else{
+                if (!options.iosApp) {
                     window.location.href = 'https://apps.apple.com/us/app/тим/id6447686674'
+                } else {
+                    tryToOpenInMultiplePhases(options.iosApp);
                 }
-                tryToOpenInMultiplePhases(urls)
+                
             } else if (hasAndroid && /Android/.test(navigator.userAgent)) {
                 var intent = options.android;
                 var intentUrl = 'intent://' + intent.host + '#Intent;' +
