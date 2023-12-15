b63939e3a357c933252ec852a78f97fc2fa89d14
Карим Фаткуллин <dilkar7685@gmail.com>
2023-12-15 15:08:13 +0300

    fixed bag with inn and kpp

--- src/utils/validation/isINNValid.ts
+++ src/utils/validation/isINNValid.ts
@@ -1,7 +1,15 @@
 const isINNValid = (orgINN: string) => {
-    if (orgINN.length !== 10 || String(+orgINN ^ 0) !== orgINN) {
+    // if (orgINN.length !== 10 || String(+orgINN ^ 0) !== orgINN) {
+    //     return false;
+    // } return true;
+
+    const innRegex = /^\d{10}$|^\d{10}$/;
+
+    if (innRegex.test(orgINN)) {
         return false;
-    } return true;
+    } else {
+        return true;
+    }
 }
 
 export default isINNValid;
--- src/utils/validation/isKPPValid.ts
+++ src/utils/validation/isKPPValid.ts
@@ -1,7 +1,15 @@
 const isKPPValid = (orgKPP: string) => {
-    if(orgKPP.length !== 9 || String(+orgKPP ^ 0) !== orgKPP) {
-        return false
-    } return true
+    // if(orgKPP.length !== 9 || String(+orgKPP ^ 0) !== orgKPP) {
+    //     return false
+    // } return true
+
+    const kppRegex = /^\d{9}$|^\d{9}$/;
+
+    if (kppRegex.test(orgKPP)) {
+        return false;
+    } else {
+        return true;
+    }
 }  
 
 export default isKPPValid;
